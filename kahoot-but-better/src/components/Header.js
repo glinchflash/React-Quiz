c5d70c75e514c72d346d28f14cfcd529ad7ce215
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-11 08:57:56 +0200

    added Header and settings components, settings not yet finished

---
+++ kahoot-but-better/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Header.css'
+
+function Header() {
+    const title = ("Quiz it up");
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}
+
+export default Header;
