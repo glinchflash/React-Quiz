57ff51306ac44710cd34524f43f5133eacd44efa
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-12 11:54:56 +0200

    showing questions

--- kahoot-but-better/src/App.js
+++ kahoot-but-better/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import Settings from "./Helpers/Settings";
 import Questions from "./components/Questions";
-import Form from "./components/Form";
+
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Form/>
             <Questions/>
         </div>
 
