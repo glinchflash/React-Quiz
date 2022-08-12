dc8cb8436494949008ba50094fd5d152763be315
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-12 11:55:38 +0200

    finished refactoring code to be able to display questions again

--- kahoot-but-better/src/components/Form.js
+++ kahoot-but-better/src/components/Form.js
@@ -1,26 +1,15 @@
 import React from "react";
-import {useState} from "@types/react";
+import Settings from "../Helpers/Settings";
 
 function Form(props) {
-    const [amount, setAmount] = useState(10)
 
-    const handleAmount = event => {
-        if (isNaN(event.target.value)) {
-            alert("The amount has to be a number, please re-enter a number")
-        } else {
-            setAmount(event.target.value)
-            return amount
-        }
-    }
     return (
         <div className="Container">
-            <form>
                 <label htmlFor="amount">How many questions do you want?</label>
-                <input type="text" name="amount" id="amount" onChange={handleAmount} required></input>
+                <input type="text" name="amount" id="amount" onChange={Settings.handleAmount} required></input>
                 <select id="categories" name="categories">
                 </select>
-                <button type={"submit"} onClick={props.getQuestions}>Submit</button>
-            </form>
+                <button onClick={props.searchQuestions}>Submit</button>
         </div>
 
     )
